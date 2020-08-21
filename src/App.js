b6c698e1da0003cfd4036ecaad8d30ca8936b820
Ashwat Mahendran <am123@snu.edu.in>
2020-08-21 15:07:46 +0530

    shop page and relevant components added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
       <div>
         <Switch>
           <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
         </Switch>
       </div>
     );
